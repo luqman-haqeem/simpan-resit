6336fdeaad28787c0c978ff29ce2c045d449f1a6
Luqman <62147517+luqman-haqeem@users.noreply.github.com>
2024-11-15 10:12:39 +0800

    add PWA support

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,6 +39,12 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+            <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+            <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+            <link rel="shortcut icon" href="/favicon.ico" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+            <meta name="apple-mobile-web-app-title" content="SimpanResit" />
+            <link rel="manifest" href="/site.webmanifest" />
             <body className="bg-gradient-to-b from-gray-100 to-gray-200 dark:from-gray-900 dark:to-gray-800">
                 {/* <ThemeProvider
                     attribute="class"
