8f8d4ce5cfc7d4e328c2d856ee803905ebe90d41
Luqman <62147517+luqman-haqeem@users.noreply.github.com>
2024-11-15 10:32:22 +0800

    fix  login page  not  center on big screen

--- app/(auth-pages)/layout.tsx
+++ app/(auth-pages)/layout.tsx
@@ -4,6 +4,6 @@ export default async function Layout({
     children: React.ReactNode;
 }) {
     return (
-        <div className="max-w-7xl flex flex-col gap-12 h-screen items-center "><div className="m-auto">{children}</div></div>
+        <div className="max-w-7xl flex flex-col gap-12 h-screen items-center max-w-full"><div className="m-auto">{children}</div></div>
     );
 }
